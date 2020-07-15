840393d86efb417ef33ba9163ecc632852054d3b
David Gerard <gerard.1787@gmail.com>
2020-07-15 10:28:45 -0400

    add vignette, version bump, add uit data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+
+
+#' Updog fits on the data from Uitdewilligen et. al. (2013)
+#'
+#' 10 SNPs from the "PGSC0003DMB000000062" super scaffold were genotyped
+#' using the \code{\link[updog]{multidog}()} function from the updog R package.
+#' These data are the resulting output.
+#'
+#' @format An object of class \code{\link[updog]{multidog}()}.
+#'     See the documentation from the updog R package.
+#'
+#' @source \url{https://doi.org/10.1371/journal.pone.0062355}
+#'
+#' @references
+#' \itemize{
+#'   \item{Uitdewilligen, Jan GAML, Anne-Marie A. Wolters, B. Bjorn, Theo JA Borm, Richard GF Visser, and Herman J. Van Eck. "A next-generation sequencing method for genotyping-by-sequencing of highly heterozygous autotetraploid potato." \emph{PloS one} 8, no. 5 (2013): e62355. \href{https://doi.org/10.1371/journal.pone.0062355}{DOI:10.1371/journal.pone.0062355}}
+#' }
+#'
+"uit"
